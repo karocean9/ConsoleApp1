f528ce653dc16c3742ce3e7511bf159575e1d17a
karthik sagar <karthiksagar9999@gmail.com>
2019-04-23 00:04:11 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public void show( int val)
+        {
+            val *= val;
+            Console.WriteLine("The value is " + val);
+        }
+        static void Main(string[] args)
+        {
+            int val = 10;
+            Program abc = new Program();
+            Console.WriteLine("Value before ref " + val);
+            abc.show(val);
+            Console.WriteLine("Value After ref " + val);
+            Console.ReadLine();
+
+        }
+    }
+}
